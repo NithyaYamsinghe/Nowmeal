443c94f8329c1b9329fe7bc8f03b2e7d66ab6797
NithyaYamsinghe <nitheeromeshi@gmail.com>
2020-04-16 14:25:45 +0530

    initial commit

---
+++ app/src/main/java/com/example/nowmeal/client/common/Common.java
@@ -0,0 +1,8 @@
+package com.example.nowmeal.client.common;
+
+import com.example.nowmeal.client.model.UserModel;
+
+public class Common {
+    public static final String USER_REFERENCES = "Users" ;
+    public static UserModel currentUser;
+}
