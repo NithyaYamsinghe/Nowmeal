29e6abfeb0d2fbbeeeac9fbc224e72dc08c19ee2
hirupiyumika <hirupiyumika@gmail.com>
2020-04-21 15:30:35 +0530

    side drawer

--- app/src/main/java/com/example/nowmeal/admin/common/Common.java
+++ app/src/main/java/com/example/nowmeal/admin/common/Common.java
@@ -5,5 +5,6 @@ import com.example.nowmeal.admin.model.ServerUserModel;
 public class Common {
 
     public static final String SERVER_REF = "Server";
+    public static final String CATEGORY_REF = "Category";
     public static ServerUserModel currentServerUser;
 }
---
+++ app/src/main/java/com/example/nowmeal/admin/ui/category/CategoryFragment.java
@@ -0,0 +1,27 @@
+package com.example.nowmeal.admin.ui.category;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.nowmeal.R;
+
+
+public class CategoryFragment extends Fragment {
+
+    private CategoryViewModel categoryViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        categoryViewModel =
+                ViewModelProviders.of(this).get(CategoryViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_admin_category, container, false);
+
+        return root;
+    }
+}
