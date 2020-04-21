15640f85cb74dcd3c30446b839784c6a79ac775f
NithyaYamsinghe <nitheeromeshi@gmail.com>
2020-04-21 11:11:14 +0530

    rating and comment implementation

--- app/src/main/java/com/example/nowmeal/client/ui/fooddetail/FoodDetailFragment.java
+++ app/src/main/java/com/example/nowmeal/client/ui/fooddetail/FoodDetailFragment.java
@@ -1,9 +1,11 @@
 package com.example.nowmeal.client.ui.fooddetail;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,27 +16,43 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.andremion.counterfab.CounterFab;
 import com.bumptech.glide.Glide;
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.example.nowmeal.R;
 import com.example.nowmeal.client.common.Common;
+import com.example.nowmeal.client.model.CommentModel;
 import com.example.nowmeal.client.model.FoodModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
+import dmax.dialog.SpotsDialog;
 
 
 public class FoodDetailFragment extends Fragment {
 
     private FoodDetailViewModel foodDetailViewModel;
     private Unbinder unbinder;
+    private android.app.AlertDialog waitingDialog;
 
     @BindView(R.id.img_food)
     ImageView img_food;
@@ -55,19 +73,156 @@ public class FoodDetailFragment extends Fragment {
     @BindView(R.id.btnShowComment)
     Button btnShowComment;
 
+    @OnClick(R.id.btn_rating)
+    void onRatingButtonClick() {
+        showDialogRating();
+    }
+
+    private void showDialogRating() {
+        androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(getContext());
+        builder.setTitle("Rating Food");
+        builder.setMessage("Please fill this information");
+        View itemView = LayoutInflater.from(getContext()).inflate(R.layout.layout_rating, null);
+
+        RatingBar ratingBar = (RatingBar) itemView.findViewById(R.id.rating_bar);
+        EditText edt_comment = (EditText) itemView.findViewById(R.id.edt_comment);
+        builder.setView(itemView);
+
+        builder.setNegativeButton("CANCEL", (dialog, which) -> {
+            dialog.dismiss();
+
+        });
+        builder.setPositiveButton("OK", (dialog, which) -> {
+            CommentModel commentModel = new CommentModel();
+            commentModel.setName(Common.currentUser.getName());
+            commentModel.setUid(Common.currentUser.getUid());
+            commentModel.setComment(edt_comment.getText().toString());
+            commentModel.setRatingValue(ratingBar.getRating());
+            Map<String, Object> serverTimeStamp = new HashMap<>();
+            serverTimeStamp.put("timeStamp", ServerValue.TIMESTAMP);
+            commentModel.setCommentTimeStamp(serverTimeStamp);
+
+            foodDetailViewModel.setCommentModel(commentModel);
+
+
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         foodDetailViewModel =
                 ViewModelProviders.of(this).get(FoodDetailViewModel.class);
         View root = inflater.inflate(R.layout.fragment_food_detail, container, false);
-       unbinder = ButterKnife.bind(this, root);
+        unbinder = ButterKnife.bind(this, root);
+        initViews();
 
         foodDetailViewModel.getMutableLiveDataFood().observe(getViewLifecycleOwner(), foodModel -> {
             displayInfo(foodModel);
 
         });
+        foodDetailViewModel.getMutableLiveDataComment().observe(getViewLifecycleOwner(), commentModel -> {
+            submitRatingToFirebase(commentModel);
+
+        });
         return root;
+    }
+
+    private void initViews() {
+
+        waitingDialog = new SpotsDialog.Builder().setCancelable(false).setContext(getContext()).build();
+
+
+    }
+
+    private void submitRatingToFirebase(CommentModel commentModel) {
+
+        waitingDialog.show();
+        // submit data to comment ref
+        FirebaseDatabase.getInstance()
+                .getReference(Common.COMMENT_REF)
+                .child(Common.selectedFood.getId())
+                .push()
+                .setValue(commentModel)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+
+                        // after submitting to comment ref
+                        addRatingToFood(commentModel.getRatingValue());
+
+                    }
+                    waitingDialog.dismiss();
+
+                });
+
+
+    }
+
+    private void addRatingToFood(float ratingValue) {
+
+        FirebaseDatabase.getInstance()
+                .getReference(Common.CATEGORY_REF)
+                .child(Common.categorySelected.getMenu_id()) // select category
+                .child("foods") // select array list food of this category
+                .child(Common.selectedFood.getKey()) // food item is a array list key is the index
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+
+                            FoodModel foodModel = dataSnapshot.getValue(FoodModel.class);
+                            foodModel.setKey(Common.selectedFood.getKey());
+
+                            // apply rating
+
+                            if (foodModel.getRatingValue() == null)
+                                foodModel.setRatingValue(0d);
+
+                            if (foodModel.getRatingCount() == null)
+                                foodModel.setRatingCount(0l);
+
+
+                            double sumRating = foodModel.getRatingValue() + ratingValue;
+                            long ratingCount = foodModel.getRatingCount() + 1;
+                            double result = sumRating / ratingCount;
+
+                            Map<String, Object> updateData = new HashMap<>();
+                            updateData.put("ratingValue", result);
+                            updateData.put("ratingCount", ratingCount);
+
+                            // update data in variable
+                            foodModel.setRatingValue(result);
+                            foodModel.setRatingCount(ratingCount);
+
+                            dataSnapshot.getRef()
+                                    .updateChildren(updateData)
+                                    .addOnCompleteListener(task -> {
+                                        waitingDialog.dismiss();
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(getContext(), "Thank You !", Toast.LENGTH_SHORT).show();
+                                            Common.selectedFood = foodModel;
+                                            foodDetailViewModel.setFoodModel(foodModel); // call refresh
+
+                                        }
+
+                                    });
+
+                        } else
+                            waitingDialog.dismiss();
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        waitingDialog.dismiss();
+                        Toast.makeText(getContext(), "" + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 
     private void displayInfo(FoodModel foodModel) {
@@ -76,8 +231,12 @@ public class FoodDetailFragment extends Fragment {
         food_description.setText(new StringBuilder(foodModel.getDescription()));
         food_price.setText(new StringBuilder(foodModel.getPrice().toString()));
 
+        if (foodModel.getRatingValue() != null)
+            ratingBar.setRating(foodModel.getRatingValue().floatValue());
+
+
         // change the title bar when click a category
-        ((AppCompatActivity)getActivity())
+        ((AppCompatActivity) getActivity())
                 .getSupportActionBar()
                 .setTitle(Common.selectedFood.getName());
 
